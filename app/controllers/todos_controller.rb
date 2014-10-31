98dc6aceeb7113203fdbdb6ce9213c5b0fcd9df9
Trevor Reiff <trevorreiff@gmail.com>
2014-10-30 22:03:28 -0400

    scoped todos to particular user

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,7 @@ class TodosController < ApplicationController
   before_filter :authenticate
 
   def index
-    @todo = Todo.all
+    @todo = Todo.where(email: session[:current_email])
   end
 
   def new
@@ -10,7 +10,7 @@ class TodosController < ApplicationController
   end
 
   def create
-    Todo.create(todo_params)
+    Todo.create(todo_params.merge(email: session[:current_email]))
     redirect_to todos_path
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030183902) do
+ActiveRecord::Schema.define(version: 20141031013626) do
 
   create_table "todos", force: true do |t|
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
 end
