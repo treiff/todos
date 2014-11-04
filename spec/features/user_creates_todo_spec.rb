8a5bdf27ceb8a5f6c8b1f70eb7afa81e700f5197
Trevor Reiff <trevorreiff@gmail.com>
2014-11-04 13:25:49 -0500

    refactor tests to use helper methods

--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -4,10 +4,8 @@ feature "user creates new todo" do
   scenario "success" do
     sign_in
 
-    click_on "Add a new todo"
-    fill_in "Title", with: "Hi, Make sure you buy bread"
-    click_on "Submit"
+    create_todo "Buy milk"
 
-    expect(page).to have_css ".todos li", text: "Hi, Make sure you buy bread"
+    expect(page).to display_todo "Buy milk"
   end
 end
